3817950ae8e44fb77fe484606acf6168cd14f478
Fatimah1403 <fattyhassan14@gmail.com>
2021-11-12 11:33:59 +0200

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  private _productsUrl = '../assets/ProductService.json';
+  private _productsUrl = '../assets/products.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
@@ -18,8 +18,8 @@ export class ProductService {
   } 
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map((response) => 
-    <Product[]>response.json) 
+    return this._http.get(this._productsUrl).map(response => 
+    <Product[]>response.json()); 
   }
     
   
